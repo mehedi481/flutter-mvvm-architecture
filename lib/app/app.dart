4ab4469a5ec485c30db61cf3785ee4159ca3b458
Mehedi Hasan <mehedihasanmax95@gmail.com>
2022-08-10 21:06:45 +0600

    creating Color Manager

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
-  MyApp._internal();
-  static final MyApp instance = MyApp._internal();
-  factory MyApp() => instance;
+  MyApp._internal();  // This is called Named Constructor
+  static final MyApp instance = MyApp._internal();  // make instance of this class
+  factory MyApp() => instance; // When call Root Class then root call will return our named constructor
 
-  int state = 0;
+  int state = 0;  // For testing we are make this variable
 
   @override
   State<MyApp> createState() => _MyAppState();
